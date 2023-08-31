a05e9eca5dd9b61797c2d8133b32066afa2adcb5
Silvio Tomatis <silviot@gmail.com>
2023-08-31 11:31:03 +0200

    Format setup.py with black

--- setup.py
+++ setup.py
@@ -2,19 +2,17 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='librocco-receipt-printer',
-    version='0.1.0',
-    url='https://github.com/librocco/receipt-printer',
-    author='Code Myriad',
-    author_email='codemyriad@example.com',
-    description='Connects to a POS printer and print receipts from a CouchDB database',
-    packages=find_packages(),    
-    install_requires=[
-        "couchdb"
-    ],
+    name="librocco-receipt-printer",
+    version="0.1.0",
+    url="https://github.com/librocco/receipt-printer",
+    author="Code Myriad",
+    author_email="codemyriad@example.com",
+    description="Connects to a POS printer and print receipts from a CouchDB database",
+    packages=find_packages(),
+    install_requires=["couchdb"],
     entry_points={
-        'console_scripts': [
-            'librocco-receipt-printer=librocco_receipt_printer.cli:main',
+        "console_scripts": [
+            "librocco-receipt-printer=librocco_receipt_printer.cli:main",
         ],
     },
 )
