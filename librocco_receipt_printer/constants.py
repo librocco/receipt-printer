f8f7f5fd6d2bb17a565570d72ed843ed94de7092
ikusteu <ivankust57@gmail.com>
2023-09-05 14:45:31 +0200

    Use temp files for printouts in standardised location

--- librocco_receipt_printer/constants.py
+++ librocco_receipt_printer/constants.py
@@ -1,7 +1,7 @@
-import os
+import tempfile
 
 COUCH_SERVER = "http://admin:admin@localhost:5000"
 
 VERSION = "v1"
 
-PRINT_OUTPUT_FILE = os.environ.get("PRINT_OUTPUT_FILE")
+PRINT_OUTPUT_FILE = tempfile.mktemp(dir="/tmp/librocco-receipt-prints")
